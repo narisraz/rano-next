622de18dc4964ebc8cb04b075251846840fcfe50
narisraz <naris.razafimahatratra@gmail.com>
2022-02-22 14:55:25 +0300

    Add .env file

--- configurations/firebase.config.ts
+++ configurations/firebase.config.ts
@@ -2,6 +2,14 @@ import firebase from "firebase/compat";
 import initializeApp = firebase.initializeApp;
 import {Firestore, getFirestore} from "@firebase/firestore";
 
-export const app = initializeApp({ /* config */ });
+export const app = initializeApp({
+  apiKey: process.env.FIREBASE_apiKey,
+  authDomain: process.env.FIREBASE_authDomain,
+  projectId: process.env.FIREBASE_projectId,
+  storageBucket: process.env.FIREBASE_storageBucket,
+  messagingSenderId: process.env.FIREBASE_messagingSenderId,
+  appId: process.env.FIREBASE_appId,
+  measurementId: process.env.FIREBASE_measurementId
+});
 
 export const FIRESTORE: Firestore = getFirestore(app);
