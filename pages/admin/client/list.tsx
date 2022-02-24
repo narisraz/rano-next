cda7a76d6b5f0519e456541ac76c3bea771dc4a4
narisraz <naris.razafimahatratra@gmail.com>
2022-02-24 16:00:34 +0300

    Add new client button

--- pages/admin/client/list.tsx
+++ pages/admin/client/list.tsx
@@ -1,8 +1,10 @@
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import AdminLayout from "../../../components/layouts/AdminLayout";
 import {FC} from "react";
 import {listClient} from "../../../configurations/di";
 import {bind} from "@react-rxjs/core";
+import Box from "@mui/material/Box";
+import AddIcon from '@mui/icons-material/Add';
 
 
 const [useClients] = bind(listClient.run(), [])
@@ -11,41 +13,47 @@ export default function ClientList() {
   const clients = useClients()
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Nom</TableCell>
-            <TableCell align="right">Téléphones</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">NIF</TableCell>
-            <TableCell align="right">Stat</TableCell>
-            <TableCell align="right">Région</TableCell>
-            <TableCell align="right">Commune</TableCell>
-            <TableCell align="right">Fokontany</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {clients.map(client =>
-            <TableRow
-              key={client.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {client.name}
-              </TableCell>
-              <TableCell align="right">{client.telephones}</TableCell>
-              <TableCell align="right">{client.email}</TableCell>
-              <TableCell align="right">{client.nif}</TableCell>
-              <TableCell align="right">{client.stat}</TableCell>
-              <TableCell align="right">{client.address?.region}</TableCell>
-              <TableCell align="right">{client.address?.commune}</TableCell>
-              <TableCell align="right">{client.address?.fokontany}</TableCell>
+    <Box>
+      <Button href={"admin/client/new"} startIcon={<AddIcon />} variant={"contained"}>
+        Nouveau
+      </Button>
+      <Box sx={{m: 2}}></Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nom</TableCell>
+              <TableCell align="right">Téléphones</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">NIF</TableCell>
+              <TableCell align="right">Stat</TableCell>
+              <TableCell align="right">Région</TableCell>
+              <TableCell align="right">Commune</TableCell>
+              <TableCell align="right">Fokontany</TableCell>
             </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {clients.map(client =>
+              <TableRow
+                key={client.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {client.name}
+                </TableCell>
+                <TableCell align="right">{client.telephones}</TableCell>
+                <TableCell align="right">{client.email}</TableCell>
+                <TableCell align="right">{client.nif}</TableCell>
+                <TableCell align="right">{client.stat}</TableCell>
+                <TableCell align="right">{client.address?.region}</TableCell>
+                <TableCell align="right">{client.address?.commune}</TableCell>
+                <TableCell align="right">{client.address?.fokontany}</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   )
 }
 
